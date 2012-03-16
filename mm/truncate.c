4a937317c41e316a0211bab8681f280b7f1dbe60
BobZhome <BobZhome@Cheerful.com>
2012-03-16 16:46:02 -0400

    Revert "fadvise support; deactivate invalidated pages, reclaim invalidated pages"
    This reverts commit b9b9d2bef32fa5163a31c078703af3d0450047e3.

--- mm/truncate.c
+++ mm/truncate.c
@@ -326,8 +326,7 @@ unsigned long invalidate_mapping_pages(struct address_space *mapping,
 {
 	struct pagevec pvec;
 	pgoff_t next = start;
-	unsigned long ret;
-	unsigned long count = 0;
+	unsigned long ret = 0;
 	int i;
 
 	pagevec_init(&pvec, 0);
@@ -354,15 +353,9 @@ unsigned long invalidate_mapping_pages(struct address_space *mapping,
 			if (lock_failed)
 				continue;
 
-			ret = invalidate_inode_page(page);
+			ret += invalidate_inode_page(page);
+
 			unlock_page(page);
-			/*
-			 * Invalidation is a hint that the page is no longer
-			 * of interest and try to speed up its reclaim.
-			 */
-			if (!ret)
-				deactivate_page(page);
-			count += ret;
 			if (next > end)
 				break;
 		}
@@ -370,7 +363,7 @@ unsigned long invalidate_mapping_pages(struct address_space *mapping,
 		mem_cgroup_uncharge_end();
 		cond_resched();
 	}
-	return count;
+	return ret;
 }
 EXPORT_SYMBOL(invalidate_mapping_pages);
 
